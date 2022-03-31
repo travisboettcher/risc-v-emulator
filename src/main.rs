218ffe90b067ab2098a41ad7d1af8111751a0099
Travis Boettcher <travis.boettcher@jamf.com>
2022-03-31 13:37:21 -0500

    Reformat to reduce duplication among different instructions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::u16;
-use crate::Instructions::*;
+use crate::Instruction::{IFormatInstruction, RFormatInstruction, UFormatInstruction};
 
 #[derive(Debug)]
 struct Register {
@@ -30,237 +29,131 @@ impl Register {
 }
 
 #[derive(Debug)]
-enum Instructions {
-    AddI {
+enum Instruction {
+    IFormatInstruction {
+        imm: i16,
         rs1: usize,
-        rd: usize,
-        imm: i16 // this should be a 12-bit signed integer
+        funct3: usize,
+        rd: usize
     },
-    Add {
+    RFormatInstruction {
+        rd: usize,
+        funct3: usize,
         rs1: usize,
         rs2: usize,
-        rd: usize
+        funct7: usize
     },
-    Slti {
-        rs1: usize,
+    UFormatInstruction {
+        imm: i32,
         rd: usize,
-        imm: i16
-    },
-    Sltiu {
-        rs1: usize,
-        rd: usize,
-        imm: i16
-    },
-    Xori {
-        rs1: usize,
-        rd: usize,
-        imm: i16
-    },
-    Ori {
-        rs1: usize,
-        rd: usize,
-        imm: i16
-    },
-    Andi {
-        rs1: usize,
-        rd: usize,
-        imm: i16
-    },
-    Lui {
-        rd: usize,
-        imm: i32
-    },
-    Auipc {
-        rd: usize,
-        imm: i32
-    },
-    Slli {
-        shamt: u16,
-        rs1: usize,
-        rd: usize
+        opcode: usize
     }
 }
 
-impl Instructions {
+impl Instruction {
     const OP_IMM: usize = 0b0010011;
     const OP: usize = 0b0110011;
     const LUI: usize = 0b0110111;
     const AUIPC: usize = 0b0010111;
 
-    fn from(bits: usize) -> Option<Instructions> {
+    fn from(bits: usize) -> Option<Instruction> {
         let opcode_mask = 0b1111111;
         let opcode = bits & opcode_mask;
         match opcode {
-            Instructions::OP_IMM => {
-                let instruction = IFormat::parse(bits);
-                match instruction.funct3 {
-                    0b000 => Some(AddI {
-                        rs1: instruction.rs1,
-                        rd: instruction.rd,
-                        imm: instruction.imm
-                    }),
-                    0b001 => Some(Slli {
-                        rs1: instruction.rs1,
-                        rd: instruction.rd,
-                        shamt: instruction.imm as u16
-                    }),
-                    0b010 => Some(Slti {
-                        rs1: instruction.rs1,
-                        rd: instruction.rd,
-                        imm: instruction.imm
-                    }),
-                    0b011 => Some(Sltiu {
-                        rs1: instruction.rs1,
-                        rd: instruction.rd,
-                        imm: instruction.imm
-                    }),
-                    0b100 => Some(Xori {
-                        rs1: instruction.rs1,
-                        rd: instruction.rd,
-                        imm: instruction.imm
-                    }),
-                    0b110 => Some(Ori {
-                        rs1: instruction.rs1,
-                        rd: instruction.rd,
-                        imm: instruction.imm
-                    }),
-                    0b111 => Some(Andi {
-                        rs1: instruction.rs1,
-                        rd: instruction.rd,
-                        imm: instruction.imm
-                    }),
-                    _ => None
-                }
-
-            },
-            Instructions::OP => {
-                let instruction = RFormat::parse(bits);
-                let funct = instruction.funct7 << 3 + instruction.funct3;
-                match funct {
-                    0b0000000000 => Some(Add {
-                        rs1: instruction.rs1,
-                        rs2: instruction.rs2,
-                        rd: instruction.rd
-                    }),
-                    _ => None
-                }
-            },
-            Instructions::LUI => {
-                let instruction = UFormat::parse(bits);
-                Some(Lui {
-                    rd: instruction.rd,
-                    imm: instruction.imm
-                })
-            },
-            Instructions::AUIPC => {
-                let instruction = UFormat::parse(bits);
-                Some(Auipc {
-                    rd: instruction.rd,
-                    imm: instruction.imm
-                })
-            }
+            Instruction::OP_IMM => Some(Instruction::parse_iformat(bits)),
+            Instruction::OP => Some(Instruction::parse_rformat(bits)),
+            Instruction::LUI => Some(Instruction::parse_uformat(bits)),
+            Instruction::AUIPC => Some(Instruction::parse_uformat(bits)),
             _ => None
         }
     }
 
     fn execute(self, register: &mut Register) {
         match self {
-            AddI { rs1, rd, imm} => {
-                let i = register.get(rs1);
-                register.put(rd, i + (imm as usize));
-            },
-            Add { rs1, rs2, rd } => {
-                let i = register.get(rs1);
-                let j = register.get(rs2);
-                register.put(rd, i + j);
-            },
-            Slti { rs1, rd, imm} => {
-                let i = register.get(rs1) as isize;
-                if i < (imm as isize) {
-                    register.put(rd, 1);
-                } else {
-                    register.put(rd, 0);
+            IFormatInstruction {funct3, rd, rs1, imm} =>
+                match funct3 {
+                    0b000 => { // Addi
+                        let i = register.get(rs1);
+                        register.put(rd, i + (imm as usize));
+                    },
+                    0b001 => { // Slli
+                        register.put(rd, register.get(rs1) << imm)
+                    },
+                    0b010 => { // Slti
+                        let i = register.get(rs1) as isize;
+                        if i < (imm as isize) {
+                            register.put(rd, 1);
+                        } else {
+                            register.put(rd, 0);
+                        }
+                    },
+                    0b011 => { // Sltiu
+                        let i = register.get(rs1);
+                        if i < (imm as usize) {
+                            register.put(rd, 1);
+                        } else {
+                            register.put(rd, 0);
+                        }
+                    },
+                    0b100 => { // Xori
+                        let i = register.get(rs1);
+                        register.put(rd, i ^ (imm as usize));
+                    },
+                    0b110 => { // Ori
+                        let i = register.get(rs1);
+                        register.put(rd, i | (imm as usize));
+                    },
+                    0b111 => { // Andi
+                        let i = register.get(rs1);
+                        register.put(rd, i & (imm as usize));
+                    },
+                    _ => return
+                },
+            RFormatInstruction {funct3, funct7, rs1, rs2, rd} => {
+                let funct = funct7 << 3 + funct3;
+                match funct {
+                    0b0000000000 => { // Add
+                        let i = register.get(rs1);
+                        let j = register.get(rs2);
+                        register.put(rd, i + j);
+                    },
+                    _ => return
                 }
             },
-            Sltiu { rs1, rd, imm} => {
-                let i = register.get(rs1);
-                if i < (imm as usize) {
-                    register.put(rd, 1);
-                } else {
-                    register.put(rd, 0);
+            UFormatInstruction {imm, rd, opcode} =>
+                match opcode {
+                    Instruction::LUI => {
+                        register.put(rd, (imm as usize) << 12);
+                    },
+                    Instruction::AUIPC => {
+                        let u_immediate = (imm as usize) << 12;
+                        register.put(rd, register.pc + u_immediate);
+                    },
+                    _ => return
                 }
-            },
-            Xori { rs1, rd, imm } => {
-                let i = register.get(rs1);
-                register.put(rd, i ^ imm);
-            },
-            Ori {rs1, rd, imm } => {
-                let i = register.get(rs1);
-                register.put(rd, i | imm);
-            },
-            Andi {rs1, rd, imm} => {
-                let i = register.get(rs1);
-                register.put(rd, i & imm);
-            },
-            Lui {rd, imm} => {
-                register.put(rd, (imm as usize) << 12)
-            },
-            Auipc {rd, imm} => {
-                let u_immediate = (imm as usize) << 12;
-                register.put(rd, register.pc + u_immediate)
-            },
-            Slli {shamt, rd, rs1 } => {
-                register.put(rd, register.get(rs1) << shamt)
-            }
         }
     }
 
-}
-
-struct IFormat {
-    imm: i16,
-    rs1: usize,
-    funct3: usize,
-    rd: usize,
-    opcode: usize
-}
-
-impl IFormat {
-    fn parse(bits: usize) -> IFormat {
-        let opcode = bits & 0b1111111;
+    fn parse_iformat(bits: usize) -> Instruction {
         let rd = bits >> 7 & 0b11111;
         let funct3 = bits >> 12 & 0b111;
         let rs1 = bits >> 15 & 0b11111;
         let imm = (bits >> 20) as i16;
-        IFormat {
+        IFormatInstruction {
             imm,
             rs1,
             funct3,
-            rd,
-            opcode
+            rd
         }
     }
-}
 
-struct RFormat {
-    opcode: usize,
-    rd: usize,
-    funct3: usize,
-    rs1: usize,
-    rs2: usize,
-    funct7: usize
-}
-
-impl RFormat {
-    fn parse(bits: usize) -> RFormat {
-        let opcode = bits & 0b1111111;
+    fn parse_rformat(bits: usize) -> Instruction {
         let rd = bits >> 7 & 0b11111;
         let funct3 = bits >> 12 & 0b111;
         let rs1 = bits >> 15 & 0b11111;
         let rs2 = bits >> 20 & 0b11111;
         let funct7 = bits >> 25;
-        RFormat {
-            opcode,
+        RFormatInstruction {
             rs1,
             rs2,
             funct3,
@@ -268,20 +161,12 @@ impl RFormat {
             rd
         }
     }
-}
 
-struct UFormat {
-    imm: i32,
-    rd: usize,
-    opcode: usize
-}
-
-impl UFormat {
-    fn parse(bits: usize) -> UFormat {
+    fn parse_uformat(bits: usize) -> Instruction {
         let opcode = bits & 0b1111111;
         let rd = bits >> 7 & 0b11111;
         let imm = (bits >> 12) as i32;
-        UFormat {
+        UFormatInstruction {
             imm,
             rd,
             opcode
@@ -302,7 +187,7 @@ fn main() {
     ];
 
     for instruction in instructions {
-        let instruction = Instructions::from(instruction).unwrap();
+        let instruction = Instruction::from(instruction).unwrap();
         println!("{:?}", instruction);
 
         instruction.execute(&mut register);
